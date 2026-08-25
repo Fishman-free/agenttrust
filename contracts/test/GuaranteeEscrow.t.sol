2183aaf316c60e99506b46e20db3d60a29731477
fishman <2156083718@qq.com>
2026-08-25 22:31:45 +0800

    style(contracts): forge fmt

--- contracts/test/GuaranteeEscrow.t.sol
+++ contracts/test/GuaranteeEscrow.t.sol
@@ -154,7 +154,7 @@ contract GuaranteeEscrowTest is Test {
 
     function test_samePrincipalTradeAndSelfGuaranteeRejected() public {
         // 注册表强制一人一社区 ID：同一责任主体无法再领第二个身份，
-    // "同主体买卖"因此在注册入口即被拒绝。
+        // "同主体买卖"因此在注册入口即被拒绝。
         vm.prank(buyer);
         vm.expectRevert(unicode"AgentRegistry: 主体已注册");
         registry.registerAgent("AlsoBuyer", "", "", _guardians());
